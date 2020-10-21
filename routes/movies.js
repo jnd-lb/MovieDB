83d36a331ecb463ccea5bf2e40510b9ee67b7173
Jihad Noureddine <jihad.nd@gmail.com>
2020-10-21 20:58:55 +0300

    step 6

--- routes/movies.js
+++ routes/movies.js
@@ -19,9 +19,35 @@ router.get("/get",(req,res,next)=>{
     });
 });
 
-router.get("/edit",(req,res,next)=>{
+router.get("/get/by-date",(req,res,next)=>{
+    movies.sort((d1,d2)=>{return d1.year - d2.year});
+    res.status(200).json({
+        status:200,
+        data:movies
+    });
 });
 
+ 
+
+router.get("/get/by-rating",(req,res,next)=>{
+    movies.sort((m1,m2)=>{return m2.rating - m1.rating});
+    res.status(200).json({
+        status:200,
+        data:movies
+    });
+});
+
+
+router.get("/get/by-title",(req,res,next)=>{
+    movies.sort((m1,m2)=>{return (m1.title>m2.title)?1:(m1.title== m2.title?0:-1)});
+    res.status(200).json({
+        status:200,
+        data:movies
+    });
+});
+
+router.get("/edit",(req,res,next)=>{
+});
 router.get("/delete",(req,res,next)=>{
 });
 
