369d5bc48c9d42a022d791424ab11265ebefff98
Jihad Noureddine <jihad.nd@gmail.com>
2020-10-25 07:54:57 +0200

    setp 13 -fix tiny bug-

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,9 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const users = [
-    {username:"jihad",password:""}
+    { username: "default", password: "$2b$10$2LxuEQWUh8Tpo.matWVcIO7joW9kPbnN1QJVSw3lGMuA3VtoRJdhW" }
 ];
+let id = 0;
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -15,20 +16,40 @@ router.post("/signin", (req, res) => {
     //TODO sanitize
     const { username, password } = req.body;
 
-    let result = !isexist(username, password);
+    let result = isexist(username, password);
+
     if (!result) return res.status(401).json({
-        status: 401, message: "User Not Found it is either the username or password is wrong", error: true
+        status: 401, message: "Username Not Found", error: true
     });
 
-    if (result == 500) return res.status(500).json({ status: 500, message: "there's an internal error" , error: true});
+    if (result == 500) return res.status(500).json({ status: 500, message: "there's an internal error", error: true });
 
+    //Result not false => user exist but now we must see if the password provided is valid
+    result.then((_result) => {
+        console.log(_result)
+        if (!_result) return res.status(401).json({
+            status: 401, message: "Wrong Password", error: true
+        });
+    
+        //In case password == password provided
+        const token = jwt.sign(
+            {
+                username: username,
+                userId: id
+            },
+            process.env.JWT_KEY,
+            {
+                expiresIn: "1h"
+            }
+        );
 
-    res.status(200).json({
-        message: "you are logged in",
-        token: result
-    });
-
+        return res.status(200).json({
+            message: "you are logged in",
+            token: token
+        });
+    })
 });
+
 
 /**
  * checks if the user name does exit in array 
@@ -37,73 +58,53 @@ router.post("/signin", (req, res) => {
  * @param {any} token. 
  */
 const isexist = (username, password) => {
-    let promise = mull;
-    let id = 0;
-    for (let i = 0; i < users.length ; i++ ) {
-                   if (users[i].username == username) {
-                // i implimented this way to not wast time hashing the password before if the username is not exist / and do not use the callback to make the code sycronously
-                promise = bcrypt.compare(password, users[id].password);
-                break
-            }
-            id = i;
-           }
-
-    // return bcrypt.hash(password, 10, (err, hash) => {
-    //     if (err) {
-    //         return 500;
-    //       }
-
-    return promise.then((err, result)=>{
-        if(err) return 500;
-        if (!result) return false;
-
-    //In case user exists
-    const token = jwt.sign(
-        {
-            username: username,
-            userId: id
-        },
-        process.env.JWT_KEY,
-        {
-            expiresIn: "1h"
+    let promise = null;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username == username) {
+            // i implimented this way to not wast time hashing the password before if the username is not exist / and do not use the callback to make the code sycronously
+            promise = bcrypt.compare(password, users[id].password);
+            break
         }
-    );
-    return token
-    })
+        id = i;
+    }
 
-    /// });
+    if (promise == null) return false; // the username does not exist 
+    return promise;
+
 }
 
 router.post('/signup', (req, res) => {
     //TODO sanitize and validate
 
-    let {password,username} = req.body;
-    console.log(username+" "+password)
+    let { password, username } = req.body;
+    console.log("Data recived:" + username + " " + password)
 
-        return bcrypt.hash(password, 10, (err, hash) => {
+    return bcrypt.hash(password, 10, (err, hash) => {
         if (err) {
             return res.status(500).json({
-                status:500,
-                message:"There's an internal error",
-                error:true
+                status: 500,
+                message: "There's an internal error",
+                error: true
             });
-          }
-        
-          users.push({username:username,password:hash});
-          console.log(users);
-          const token = jwt.sign(
+        }
+
+        console.log("Hash:" + hash.toString());
+
+        users.push({ username: username, password: hash });
+        console.log(users);
+        const token = jwt.sign(
             {
                 username: username,
-                userId: users.length-1 // i'm taking the index of the elements in array as id
+                userId: users.length - 1 // i'm taking the index of the elements in array as id
             },
             process.env.JWT_KEY,
             {
                 expiresIn: "1h"
             }
         );
-        return res.status(201).json({status:201,message:"signed up successfully",username:username,token: "Bearer "+token});
+        return res.status(201).json({ status: 201, message: "signed up successfully", username: username, token: "Bearer " + token });
 
-        });
+    });
 
 });
 
